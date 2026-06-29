ac0aea13c1c3cf495dbfa4d9c638e07343487be4
Paul Alarba <alarbapaulax@gmail.com>
2026-06-30 03:03:37 +0800

    add comment for creating db

--- Alarba1/dp.php
+++ Alarba1/dp.php
@@ -1,4 +1,17 @@
 <?php
+/*
+CREATE DATABASE registrationdb
+USE registrationdb
+
+CREATE TABLE Registration (
+    idNum VARCHAR(255) PRIMARY KEY,
+    campus VARCHAR(255),
+    studFName VARCHAR(255),
+    studLName VARCHAR(255),
+    amountPaid DECIMAL(10, 2),
+    attended VARCHAR(10)
+);
+*/
 $host = "localhost";
 $user= "root";
 $pass = "PaulAlarbaDB2004";
